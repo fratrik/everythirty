003b9cefd507ec68e9133b6bfc32b5847a24a7ee
Craig Fratrik <cfratrik@gmail.com>
2011-06-06 16:44:27 -0700

    import

---
+++ README.md
@@ -0,0 +1,9 @@
+== History ==
+I read [this story][] about blocking out one's time in 30 minute chunks. I wanted a way of knowing when the transition happens, and I wanted to do something with [node.js][].
+
+== Dependencies ==
+* node.js
+* espeak
+
+[this story]: http://the99percent.com/tips/7032/A-Day-Without-Distraction-Lessons-Learned-from-12-Hrs-of-Forced-Focus
+[node.js]: http://nodejs.org/
---
+++ everythirty.js
@@ -0,0 +1,31 @@
+var exec = require('child_process').exec
+
+var unit = 15 * 60;
+
+function cur_time(c) {
+    exec('date +%s',
+        function (error, stdout) {
+            c(stdout.trim());
+        })
+}
+
+function speak_time(time, c) {
+    exec('date -d @'+time+' +\'%l, %M\'',
+        function (error, stdout) {
+            console.log(stdout.trim())
+            exec('espeak "'+stdout+'"', c)
+        })
+}
+
+function schedule() {
+    cur_time(function (time) {
+        var rounded = Math.ceil(time / unit) * unit
+        setTimeout(speak_time,
+            1000 * (rounded - time),
+            rounded,
+            schedule)
+    })
+}
+
+cur_time(speak_time)
+schedule()
